fa37d51695c530f2b98eeab3c661052068f584f9
Babul <kbhagatb@gmail.com>
2024-07-29 17:30:50 +0530

    implement map

--- controller/listinngs.js
+++ controller/listinngs.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 module.exports.index = async (req, res) => {
   try {
     const allListings = await Listing.find({});
@@ -30,11 +34,22 @@ module.exports.showroute = async (req, res) => {
   // console.log(listing);
   res.render("listings/show.ejs", { listing });
 };
+
 //create listing
 module.exports.post_new = async (req, res, next) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.dat.location,
+      limit: 1,
+
+    })
+    .send();
+    console.log(response.body.features[0].geometry);
+    res.send("Done!");
+
   let url = req.file.path;
   let filename = req.file.filename;
-  console.log(url, "...", filename);
+  // console.log(url, "...", filename);
   const newListing = new Listing(req.body.dat);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
@@ -48,15 +63,14 @@ module.exports.post_new = async (req, res, next) => {
 
 //edit_from
 module.exports.edit_form = async (req, res) => {
-
   const { id } = req.params;
   const listing = await Listing.findById(id);
   if (!listing) {
     throw new ExpressError(404, "Listing not found");
   }
-  let originalImageUrl=listing.image.url;
-  originalImageUrl=originalImageUrl.replace("/upload","/upload/h_300,w_250");
-  res.render("listings/edit.ejs", { listing,originalImageUrl });
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
 // update listing
